311ad48572bbb30050a686c425b8bde5de2db64e
jonjpeterkin <jonjpeterkin@gmail.com>
2020-11-18 22:44:37 -0500

    almost MVP

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const firebaseAdmin = require('firebase-admin');
 const { App } = require('@slack/bolt');
 
+const port = process.env.PORT || 3000;
+
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
@@ -24,29 +26,87 @@ function getFirebaseServiceAccount () {
   }
 }
 
-// Stores the data from the Firebase DB. TODO: Refresh the snapshot periodically
-const terms = getTerms();
+// Store for the data from the Firebase DB. TODO: Refresh the snapshot periodically
+const store = {
+  terms: []
+};
+
+// Message handler
+app.message(async ({ payload, message, client, say }) => {
+  if (message.hidden === true) return;
+
+  switch (message.subtype) {
+    // Whitelist message subtypes to respond to
+    case undefined:
+    case 'message_replied': // TODO: respond in thread
+      break;
+    default:
+      // Do nothing if subtype is not whitelisted
+      return;
+  }
+
+  const matches = [];
+  store.terms.forEach((term) => {
+    if (message.text.includes(term.term)) {
+      matches.push(term);
+    }
+  });
+
+  if (matches.length > 0) {
+    console.log('matches found:', matches);
+
+    const responseBase = `*Whoa there!* Looks like you're making alphabet soup. \nI can help translate:`;
+
+    const meaningList = matches.reduce((acc, cur) => {
+      const definition = `\n• _${cur.term}_: *${cur.definition}*`;
+      const learnMore = cur.link ? ` (<${cur.link}|learn more>)` : '';
+      return acc + definition + learnMore;
+    }, '');
+
+    await say({
+      unfurl_links: false,
+      text: responseBase + meaningList,
+    });
+  }
+
+  // try {
+  //   // Call the chat.postEphemeral method using the built-in WebClient
+  //   // The client uses the token you used to initialize the app
+  //   const result = await app.client.chat.postEphemeral({
+  //     // Payload message should be posted in the channel where original message was heard
+  //     channel: payload.channel,
+  //     // The user the message should appear for
+  //     user: payload.user,
+  //     text: "Shhhh only you can see this :shushing_face:"
+  //   });
+  //   console.log(payload)
+  //   console.log(result);
+  // }
+  // catch (error) {
+  //   console.error(error);
+  // }
+});
+
+(async () => {
+  // Start the app
+  await app.start(port);
+
+  // Get data from firebase
+  store.terms = await getTerms();
+
+  console.log('Jargon Jar is running at port', port);
+})();
 
 async function getTerms () {
   try {
     // Couldn't we have used the firebase rest API? --> Yes.
     const snapshot = await database.ref().once('value');
-    // console.log('snapshot:', snapshot.val());
+    console.log('Terms fetched from Firebase');
     return snapshot.val();
   }
   catch (error) {
+    console.error(error);
     console.error('Failed to get terms from Firebase');
-    throw error;
+    return [];
   }
 }
-
-// Message handler
-app.message(async ({ message, say }) => {
-  // await say('something');
-});
-
-(async () => {
-  // Start the app
-  await app.start(process.env.PORT || 3000);
-  console.log('⚡️ Bolt app is running!');
-})();
